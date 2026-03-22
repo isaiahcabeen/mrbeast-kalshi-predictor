78f5ad45d114b108f720d4b00258233dcf3b7895
Isaiah Cabeen <157175009+isaiahcabeen@users.noreply.github.com>
2026-03-22 14:34:35 -0500

    Update KALSHI_BASE_URL to use new domain

--- lib/kalshi.ts
+++ lib/kalshi.ts
@@ -1,7 +1,7 @@
 import crypto from "crypto";
 import { WORDS } from "./words";
 
-const KALSHI_BASE_URL = process.env.KALSHI_BASE_URL ?? "https://api.kalshi.co";
+const KALSHI_BASE_URL = process.env.KALSHI_BASE_URL ?? "https://api.kalshi.com";
 const KALSHI_KEY_ID = process.env.KALSHI_API_KEY_ID;
 const KALSHI_PRIVATE_KEY = (process.env.KALSHI_PRIVATE_KEY ?? "").replace(/\\n/g, "\n");
 #new
