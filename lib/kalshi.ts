9b4642b713a619ca7d2f575f6075ef9e60d6ef04
Isaiah Cabeen <157175009+isaiahcabeen@users.noreply.github.com>
2026-03-22 14:31:30 -0500

    Add KalshiMarketPrice interface to kalshi.ts

--- lib/kalshi.ts
+++ lib/kalshi.ts
@@ -4,7 +4,7 @@ import { WORDS } from "./words";
 const KALSHI_BASE_URL = process.env.KALSHI_BASE_URL ?? "https://api.kalshi.co";
 const KALSHI_KEY_ID = process.env.KALSHI_API_KEY_ID;
 const KALSHI_PRIVATE_KEY = (process.env.KALSHI_PRIVATE_KEY ?? "").replace(/\\n/g, "\n");
-
+#new
 export interface KalshiMarketPrice {
   word: string;
   ticker: string;
